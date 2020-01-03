f9c80e7cf786021529573b55606bf2459f7bca30
Carl Joseph <cljoseph@yahoo.co.uk>
2020-01-03 12:42:12 +0000

    Add project files.

---
+++ CodetoHTMLMarkup/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace CodetoHTMLMarkup
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.codeToConvert = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Result = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // codeToConvert
+            // 
+            this.codeToConvert.Location = new System.Drawing.Point(12, 46);
+            this.codeToConvert.Multiline = true;
+            this.codeToConvert.Name = "codeToConvert";
+            this.codeToConvert.Size = new System.Drawing.Size(776, 136);
+            this.codeToConvert.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Code to convert to HTML Markup";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 188);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(335, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Convert HTML markup  to HTML markup ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.ConvertHTML);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(626, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Cut and paste code into this box ";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(453, 188);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(335, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Convert C# code to HTML markup";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.ConvertCsharp);
+            // 
+            // Result
+            // 
+            this.Result.Location = new System.Drawing.Point(12, 246);
+            this.Result.Multiline = true;
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(776, 118);
+            this.Result.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 370);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(776, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Copy Result to Clipboard";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.resultToClipboard);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 230);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Result";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(665, -2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(123, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "About";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.About);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 409);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.codeToConvert);
+            this.Name = "Main";
+            this.Text = "Code to HTML Markup";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox codeToConvert;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox Result;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ CodetoHTMLMarkup/Form1.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CodetoHTMLMarkup
+{
+    public partial class Main : Form
+    {
+        private List<string> keywordsCsharp = new List<string>()
+        {
+           "abstract", "as", "base", "bool", "break", "byte", "case", "catch", "char",
+           "checked", "class", "const","continue", "decimal", "default", "delegate","do",
+           "double", "else", "enum","event", "explicit", "extern", "false", "finally",
+           "fixed", "float", "for","foreach", "goto", "if", "implicit","in", "int",
+           "interface", "internal","is", "lock", "long", "namespace", "new", "null",
+           "object", "operator","out", "override", "params", "private","protected",
+           "public", "readonly", "ref","return", "sbyte", "sealed", "short","sizeof",
+           "stackalloc", "static", "string","struct", "switch", "this", "throw","true",
+           "try", "typeof", "uint","ulong", "unchecked", "unsafe", "ushort","using",
+           "static", "virtual", "void","volatile", "while",
+        };
+
+        private Random generator = new Random();
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private string SetColour(string str, string colour) 
+        {
+            string ToReturn = "<span style = \"color:" + colour + "\">";
+            ToReturn += str;
+            ToReturn += "</span>";
+            return ToReturn;
+        }
+
+        private string SetStart(string Elementid , string title)
+        {          
+            string ToReturn = "";           
+            ToReturn += "<div style = \"border:2px solid black \">" + System.Environment.NewLine;
+            ToReturn += "    <div style = \"border-bottom:2px solid black; padding:5px\">" + System.Environment.NewLine;
+            ToReturn += "        <div class=\"row\">" + System.Environment.NewLine;
+            ToReturn += "            <div class=\"col-md-6\">" + System.Environment.NewLine;
+            ToReturn += "           <h6>"+ title+ "</h6>" + System.Environment.NewLine;
+            ToReturn += "       </div>" + System.Environment.NewLine;
+            ToReturn += "       <div class=\"col-md-6\">" + System.Environment.NewLine;
+            ToReturn += "           <button class=\"float-right\" onclick=\"Copy" + Elementid + "()\">Copy</button>" + System.Environment.NewLine;
+            ToReturn += "       </div>" + System.Environment.NewLine;
+            ToReturn += "   </div>" + System.Environment.NewLine;
+            ToReturn += "</div>" + System.Environment.NewLine;
+            ToReturn += "<div style = \"padding:5px\" >" + System.Environment.NewLine;
+            ToReturn += "<pre><code id=\"" + Elementid + "\">" + System.Environment.NewLine;
+            return ToReturn;
+        }
+        private string SetEnd(string Elementid)
+        {
+            string ToReturn = "";
+            ToReturn += "</code></pre>" + System.Environment.NewLine;
+            ToReturn += "</div>" + System.Environment.NewLine;
+            ToReturn += "</div>" + System.Environment.NewLine;
+            ToReturn += "<script>" + System.Environment.NewLine;
+            ToReturn += " function Copy" + Elementid + " ()" + System.Environment.NewLine;
+            ToReturn += " {" + System.Environment.NewLine; ;
+            ToReturn += "  node = document.getElementById(\'" + Elementid + "\');" + System.Environment.NewLine;
+            ToReturn += "  const selection = window.getSelection();" + System.Environment.NewLine;
+            ToReturn += "  const range = document.createRange();" + System.Environment.NewLine;
+            ToReturn += "  range.selectNodeContents(node);" + System.Environment.NewLine;
+            ToReturn += "  selection.removeAllRanges();" + System.Environment.NewLine;
+            ToReturn += "  selection.addRange(range);" + System.Environment.NewLine;
+            ToReturn += "  document.execCommand(\"copy\");" + System.Environment.NewLine;
+            ToReturn += " }" + System.Environment.NewLine;
+            ToReturn += "</script>" + System.Environment.NewLine;
+            return ToReturn;
+        }
+
+
+        private string convertToHtmlMarkup(string text) 
+        {
+            string toReturn = "";
+            Boolean PassedFirstElement = false;
+            Boolean OpenQuote = false;
+            string lastChar = "";
+            foreach (char ch in text)
+            {
+                switch (ch)
+                {
+                    case '<':
+                        if (lastChar == "Char") { toReturn += "</span>"; }
+                        toReturn += SetColour("&lt;", "blue");
+                        lastChar = "LessThan";
+                        PassedFirstElement = true;
+                        break;
+                    case '>':
+                        if (lastChar == "Char") { toReturn += "</span>"; }
+                        toReturn += SetColour("&gt;", "blue");
+                        lastChar = "GreaterThan";
+                        break;
+                    case ' ':
+                        if (lastChar == "Char" && !OpenQuote) { toReturn += "</span>"; }
+                        toReturn += "&nbsp;";
+                        lastChar = "Space";
+                        break;
+                    case '@':
+                        if (lastChar == "Char") { toReturn += "</span>"; }
+                        toReturn += SetColour("&#64;", "blue");
+                        lastChar = "At";
+                        break;
+                    case '=':
+                        if (lastChar == "Char") { toReturn += "</span>"; }
+                        toReturn += SetColour("=", "blue");
+                        lastChar = "Equal";
+                        break;
+                    case '"':
+                        if (OpenQuote) { OpenQuote = false; } else { OpenQuote = true; }
+                        if (lastChar == "Char") { toReturn += "</span>"; }
+                        toReturn += SetColour("\"", "blue");
+                        lastChar = "Quote";
+                        break;
+                    case '{':
+
+                        if (lastChar == "Char") { toReturn += "</span>"; }
+                        toReturn += SetColour("{", "blue");
+                        lastChar = "OpenBrace";
+                        break;
+                    case '}':
+                        if (lastChar == "Char") { toReturn += "</span>"; }
+                        toReturn += SetColour("}", "blue");
+                        lastChar = "CloseBrace";
+                        break;
+                    case ',':
+
+                        if (lastChar == "Char") { toReturn += "</span>"; }
+                        toReturn += SetColour(",", "blue");                      
+                        lastChar = "comma";
+                        break;
+                    default:
+                        if (PassedFirstElement)
+                        {
+                            switch (lastChar)
+                            {
+                                case "LessThan":
+                                    toReturn += "<span style = \"color:brown\">";
+                                    break;
+                                case "GreaterThan":
+                                    toReturn += "<span style = \"color:black\">";
+                                    break;
+                                case "Space":
+                                    if (!OpenQuote)
+                                    {
+                                        toReturn += "<span style = \"color:black\">";
+                                        break;
+                                    }
+                                    break;
+                                case "At":
+                                    toReturn += "<span style = \"color:blue\">";
+                                    break;
+                                case "Equal":
+                                    toReturn += "<span style = \"color:black\">";
+                                    break;
+                                case "Quote":
+                                    toReturn += "<span style = \"color:black\">";
+                                    break;
+                                case "OpenBrace":
+                                    toReturn += "<span style = \"color:black\">";
+                                    break;
+                                case "CloseBrace":
+                                    toReturn += "<span style = \"color:black\">";
+                                    break;
+                                case "comma":
+                                    toReturn += "<span style = \"color:black\">";
+                                    break;
+                            }
+                            lastChar = "Char";
+                        }
+                        toReturn += ch;
+                        break;
+                }
+            }
+            return toReturn;
+        }
+
+        private string convertCsharpToHtmlMarkup(string text) 
+        {
+            string toReturn = "";
+
+            for (int ct = 0; ct <= text.Length - 1; ct++)
+            {
+                switch (text[ct])
+                {
+                    case ' ':
+                        toReturn += "&nbsp;";
+                        break;
+                    case '"':
+                        toReturn += SetColour("\"", "brown");
+                        break;
+                    case '{':
+                        toReturn += SetColour("{", "brown"); 
+                        break;
+                    case '}':
+                        toReturn += SetColour("}", "brown");
+                        break;
+                    case '[':
+                        toReturn += SetColour("[", "brown");
+                        break;
+                    case ']':
+                        toReturn += SetColour("]", "brown");
+                        break;
+                    case '(':
+                        toReturn += SetColour("(", "brown");
+                        break;
+                    case ')':
+                        toReturn += SetColour(")", "brown");
+                        break;
+                    default:
+                        // now check is this the start of a keyword
+                        string match = "";
+                        foreach (string word in keywordsCsharp)
+                        {
+                            // if first characters match
+                            if (text[ct] == word[0])
+                            {
+                                // if word does not exceed length of string from ct                              
+                                if (word.Length <= (text.Length - ct))
+                                {
+                                    for (int i = 0; i <= word.Length - 1; i++)
+                                    {
+                                        if (word[i] != text[ct + i])
+                                        {
+                                            break;
+                                        }
+                                        if ((i == word.Length - 1) && (text[ct + word.Length] == ' '))
+                                        {
+                                            match = word;
+                                        }
+                                    }
+                                }
+                            }
+                            if (match != "") { break; }
+                        }
+                        if (match.Length > 1)
+                        {
+                            toReturn += "<span style = \"color:blue\">" + match + "</span>";
+                            ct = ct + match.Length - 1;
+                        }
+                        else
+                        {
+                            toReturn += text[ct];
+                        }
+                        break;
+                }
+            }
+            return toReturn;
+        }
+
+
+        private string HTMlconverter(string text)
+        {
+            string toReturn = "";
+            string Elementid = "HTMLid"+ generator.Next(1, 999999).ToString().PadLeft(5, '0');
+            //+ Convert.ToBase64String(Guid.NewGuid().ToByteArray()).Substring(0, 8);
+            toReturn += SetStart(Elementid," HTMl Markup");
+            toReturn += convertToHtmlMarkup(text);
+            toReturn += SetEnd(Elementid);
+            return toReturn;
+        }
+
+        private string cSharpConverter(string text)
+        {
+            string toReturn = "";
+            string Elementid = "cSharpid" + generator.Next(1, 999999).ToString().PadLeft(5, '0');
+            //Convert.ToBase64String(Guid.NewGuid().ToByteArray()).Substring(0, 8);
+            toReturn += SetStart(Elementid, " C# Markup");
+            toReturn += convertCsharpToHtmlMarkup(text);
+            toReturn += SetEnd(Elementid);
+            return toReturn;
+
+        }
+
+        private void ConvertHTML(object sender, EventArgs e)
+        {
+            Result.Text = HTMlconverter(codeToConvert.Text);
+        }
+ 
+        private void ConvertCsharp(object sender, EventArgs e)
+        {
+            Result.Text = cSharpConverter(codeToConvert.Text);
+        }
+
+        private void resultToClipboard(object sender, EventArgs e)
+        {
+            Clipboard.SetText(Result.Text);
+        }
+
+        private void About(object sender, EventArgs e)
+        {
+            string text = "";
+            text = "This app takes code in HTML or C#  and converts it to a form that can be shown "; 
+            text += "in a HTML page.  ";
+            var result = MessageBox.Show(text,"About", MessageBoxButtons.OK);
+        }
+    }
+}
